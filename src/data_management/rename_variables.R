11403d9b28ffb28d56034685eb7c15b7470d65c0
Alex Jenni <alexandrejenni@gmail.com>
2019-02-19 16:56:21 +0000

    Generated new variables and finished renaming them. Added the .gitignore file

--- src/data_management/rename_variables.R
+++ src/data_management/rename_variables.R
@@ -39,18 +39,44 @@ print("Loading data")
 cps_data <- read_csv("src/data/cps_77-93_men.csv")
 
 # Rename variables
-print("Rename variables")
+print("Rename and generate variables")
 cps_data <- cps_data %>%
-              rename(non_oil = n,
-                     oecd = o,
-                     intermediate = i,
-                     gdp_60 = rgdpw60,
-                     gdp_85 = rgdpw85,
-                     gdp_growth_60_85 = gdpgrowth,
-                     pop_growth_60_85 = popgrowth,
-                     inv_gdp = i_y,
-                     school = school)
+    setNames(tolower(names(cps_data))) %>% 
+    select(year,
+           cpi99,
+           age,
+           statefip,
+           msa_code=metarea,
+           weights=asecwt,
+           hispan,
+           empstat,
+           labforce,
+           educ,
+           weeks_worked=wkswork1,
+           wage_inc = incwage)
+
+
+# Generate variables
+cps_data <- cps_data %>%
+  mutate(miami= msa_code==5000) %>% 
+  mutate(hispanic = hispan < 900 & hispan > 0) %>% 
+  mutate(year = year -1) %>%  # corresponds to the previous year income
+  mutate(educ_group=educ)
+
+cps_data$educ_group[cps_data$educ<61] <- 1
+cps_data$educ_group[cps_data$educ>=70 & cps_data$educ<75] <- 2
+cps_data$educ_group[cps_data$educ>=80 & cps_data$educ<101] <- 3
+cps_data$educ_group[cps_data$educ>=110] <- 4
+cps_data$educ_group <- ordered(cps_data$educ_group,
+  levels = c(1,2,3,4),
+  labels = c("<12 years", "12 years", "13-15 years", ">15 years"))
+cps_data <- cps_data %>%
+  select(-educ)
+
+# Account for missings
+cps_data$hispanic[cps_data$hispan>900] <- NA
+
 
 # Save data
 print("saving output")
-write_csv(mrw_data, opt$out)
+write_csv(cps_data, "out/data/cps_borjas_renamed.csv")
